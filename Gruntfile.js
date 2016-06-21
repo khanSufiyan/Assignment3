c388586ca4e0aca7d284dc1ce8e37d85fe519c1c
khanSufiyan <sufiyan.khan.1886@gmail.com>
2016-06-21 16:02:53 +0530

    todo app

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,23 +4,52 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-     concat: {
-        user:{
-       dest:'js/user.js',
-        src: [
+  concat: {
+    user:{
+      dest:'js/user.js',
+      src: [
+      'js/models/TodoModel.js',
+      'js/collections/TodoCollection.js',
+      'js/views/TodoView.js',
+      'js/views/TodoListView.js',
+      ]
+    }
+  },
 
-  'js/models/TodoModel.js',
-  'js/collections/TodoCollection.js',
-  'js/views/TodoView.js',
-  'js/views/TodoListView.js',
-          ]
+  /*concat: {
+    vendor:{
+      dest:'js/vendor.js',
+      src: [
+      'js/libs/jquery/jquery-min.js',
+      'js/libs/bootstrap/bootstrap.min.js',
+      'js/libs/backbone/backbone-min.js',
+      'js/libs/underscore/underscore-min.js',
+      'js/libs/handlebars/handlebars.js',
+      'js/libs/backbone.localstorage/backbone.localStorage.js',
+
+      ]
+    }
+  },*/
+
+less: {
+      development: {
+        options: {
+          paths: ['less']
+        },
+        files: {
+          'css/main.css': 'less/main.less',
+        }
       }
-     },
+    },
+
+
+
    });
 
   // task plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
    // tasks
-  grunt.registerTask('default', [ 'concat' ]);
+  grunt.registerTask('default', [ 'concat' ,'less']);
 
 };
--- js/user.js
+++ js/user.js
@@ -17,12 +17,16 @@ var TodoCollecetion = Backbone.Collection.extend({
 });
 var TodoView = Backbone.View.extend({
     tagName: 'div',
-    className:'todo-div',
-    template: _.template($('#item-template').html()),
+    className:'todo-div row',
+
     events: {
       'click #comp': 'toggleCompleted',
       'click #undo': 'toggleCompleted',
-      'click #delete': 'clear',
+      'dblclick #todo-title': 'edit',
+      'keypress .edit': 'updateOnEnter',
+      'keydown .edit': 'revertOnEscape',
+      'blur .edit': 'close',
+      'click #delete': 'clear'
     },
       initialize: function () {
      this.model.on('change', this.render, this);
@@ -30,20 +34,68 @@ var TodoView = Backbone.View.extend({
     },
 
     render: function(){
-      this.$el.html(this.template(this.model.toJSON()));
+
+      var source = $('#item-template').html();
+      var template = Handlebars.compile(source);
+      var html = template(this.model.toJSON());
+      this.$el.html(html);
+
       this.$("#comp").toggleClass('hidden', this.model.get('completed'));
       this.$("#undo").toggleClass('hidden', !(this.model.get('completed')));
-
       this.$("#todo-title").toggleClass('completed', this.model.get('completed'));
+
       return this;
+
     },
+
     toggleCompleted: function () {
       this.model.toggle();
     },
+
     clear: function () {
       this.model.destroy();
-    }
+    },
 
+    edit: function () {
+      this.$("#todo-title").addClass('hidden');
+      this.$("#edit").addClass('editing');
+      this.$("#editinput").focus();
+    },
+
+    updateOnEnter: function (e) {
+      if (e.which === 13) {
+        this.close();
+      }
+    },
+
+    revertOnEscape: function (e) {
+      if (e.which === 27) {
+        this.$("#todo-title").removeClass('hidden');
+        this.$("#edit").removeClass('editing');
+        this.$("#editinput").val(this.model.get('title'));
+      }
+    },
+
+    close: function () {
+      var value = this.$("#editinput").val();
+      var trimmedValue = value.trim();
+
+
+      if (!this.$("#edit").hasClass('editing')) {
+        return;
+      }
+
+      if (trimmedValue) {
+        this.model.save({ title: trimmedValue });
+      }
+      else {
+        this.clear();
+      }
+
+      this.$("#edit").removeClass('editing');
+      this.$("#todo-title").removeClass('hidden');
+
+    },
 
   });
 
