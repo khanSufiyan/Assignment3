dfed03a14c2de5551041c89ee5cb4551acc247be
khanSufiyan <sufiyan.khan.1886@gmail.com>
2016-06-28 16:45:56 +0530

    üpdates

--- js/user.js
+++ js/user.js
@@ -122,13 +122,18 @@ var TodoListView = Backbone.View.extend({
       initialize: function () {
         todoCollecetion = new TodoCollecetion();
 
+        $('[data-toggle="tooltip"]').tooltip();
+
         this.input = this.$('#todo_input');
+        this.allCheckbox = this.$('.toggle-all')[0];
         todoCollecetion.on('add', this.addAll, this);
         todoCollecetion.on('reset', this.addAll, this);
         todoCollecetion.fetch();
       },
       events: {
-        'keypress #todo_input': 'createTodoOnEnter'
+        'keypress #todo_input': 'createTodoOnEnter',
+        'click .toggle-all': 'toggleAllComplete',
+        'click .clrCompleted': 'clrCompleted',
       },
       createTodoOnEnter: function(e){
         if ( e.which !== 13 || !this.input.val().trim() ) {
@@ -152,7 +157,20 @@ var TodoListView = Backbone.View.extend({
           title: this.input.val().trim(),
           completed: false
         }
-      }
+      },
+      toggleAllComplete: function () {
+      var completed = this.allCheckbox.checked;
+
+        todoCollecetion.each(function (TodoModel) {
+        TodoModel.save({
+          completed: completed
+        });
+      });
+    },
+   clrCompleted: function () {
+      _.invoke(todoCollecetion.completed(), 'destroy');
+      return false;
+    },
       });
 
 var todoListView = new TodoListView();
